fb041c3653b83d33c014d4d44317e6796a6d00b7
Chethana <56347342+chethana613@users.noreply.github.com>
2024-03-23 13:15:34 -0700

    Updates

--- sorting_algorithms/heap_sort.py
+++ sorting_algorithms/heap_sort.py
@@ -1,31 +1,32 @@
-def heapify(arr, N, i):
-	largest = i # Initialize largest as root
-	l = 2 * i + 1	 # left = 2*i + 1
-	r = 2 * i + 2	 # right = 2*i + 2
+#Heap sort implementation
 
-	if l < N and arr[largest] < arr[l]:
+def heapify(arr, n, i):
+	largest = i 
+	l = 2 * i + 1	 
+	r = 2 * i + 2	 
+
+	if l < n and arr[largest] < arr[l]:
 		largest = l
 
-	if r < N and arr[largest] < arr[r]:
+	if r < n and arr[largest] < arr[r]:
 		largest = r
 
 	if largest != i:
 		arr[i], arr[largest] = arr[largest], arr[i] # swap
 
-		# Heapify the root.
-		heapify(arr, N, largest)
-
+		# Heapifying the root.
+		heapify(arr, n, largest)
 
 
 def heap_sort(arr):
-	N = len(arr)
+	n = len(arr)
 
-	# Build a maxheap.
-	for i in range(N//2 - 1, -1, -1):
-		heapify(arr, N, i)
+	# Building a maxheap.
+	for i in range(n//2 - 1, -1, -1):
+		heapify(arr, n, i)
 
-	# One by one extract elements
-	for i in range(N-1, 0, -1):
+	# Extracting elements
+	for i in range(n-1, 0, -1):
 		arr[i], arr[0] = arr[0], arr[i] # swap
 		heapify(arr, i, 0)
 
