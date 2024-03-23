d4c2b3e391688bcaf6a5bc56b8e37436373d23aa
Chethana <56347342+chethana613@users.noreply.github.com>
2024-03-23 13:13:21 -0700

    Updates

--- sorting_algorithms/merge_sort.py
+++ sorting_algorithms/merge_sort.py
@@ -1,27 +1,28 @@
+#Merge sort implementation
 
 def merge_sort(arr):
 	if len(arr) > 1:
 		mid = len(arr)//2
-		L = arr[:mid]
-		R = arr[mid:]
-		merge_sort(L)
-		merge_sort(R)
+		left = arr[:mid]
+		right = arr[mid:]
+		merge_sort(left)
+		merge_sort(right)
 
 		i = j = k = 0
-		while i < len(L) and j < len(R):
-			if L[i] <= R[j]:
-				arr[k] = L[i]
+		while i < len(left) and j < len(right):
+			if left[i] <= right[j]:
+				arr[k] = left[i]
 				i += 1
 			else:
-				arr[k] = R[j]
+				arr[k] = right[j]
 				j += 1
 			k += 1
-		while i < len(L):
-			arr[k] = L[i]
+		while i < len(left):
+			arr[k] = left[i]
 			i += 1
 			k += 1
 
-		while j < len(R):
-			arr[k] = R[j]
+		while j < len(right):
+			arr[k] = right[j]
 			j += 1
 			k += 1
